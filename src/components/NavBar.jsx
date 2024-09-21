7e0a65cb986ce92083b1bf30c4c82704c6e70c07
Soma Hargitai <>
2024-09-21 19:31:11 +0200

    fix noun plurals

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -44,7 +44,7 @@ function NavBar({ menuOptions }) {
             }}
           />
         </IconButton>
-        v1.1.1
+        v1.1.2
         <IconButton
           edge="end"
           color="inherit"
